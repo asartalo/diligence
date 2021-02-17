9aaa5e712095d6d2ee5de133f3a3a575dea924f9
Wayne Duran <asartalo@gmail.com>
2021-02-17 19:07:41 +0800

    chore: update sqflite_common_ffi dependency

--- packages/sqflite_common_ffi/lib/sqflite_ffi.dart
+++
@@ -1,8 +0,0 @@
-library sqflite_common_ffi;
-
-export 'src/sqflite_ffi.dart'
-    show
-        SqfliteFfiInit,
-        databaseFactoryFfi,
-        createDatabaseFactoryFfi,
-        sqfliteFfiInit;
