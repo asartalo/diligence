ba04f3289f2ae30f7c2aa77bfa05dd7a392a09a0
Wayne Duran <asartalo@gmail.com>
2025-03-30 12:05:14 +0800

    fix: upgraded project

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -23,22 +23,22 @@ MaterialColor createMaterialColorFromRgb(int red, int green, int blue) {
 MaterialColor createMaterialColor(Color color) {
   final strengths = <double>[.05];
   final Map<int, Color> swatch = {};
-  final r = color.red;
-  final g = color.green;
-  final b = color.blue;
+  final r = color.r;
+  final g = color.g;
+  final b = color.b;
 
   for (int i = 1; i < 10; i++) {
     strengths.add(0.1 * i);
   }
   for (final strength in strengths) {
     final double ds = 0.5 - strength;
-    swatch[(strength * 1000).round()] = Color.fromRGBO(
-      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
-      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
-      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
-      1,
+    swatch[(strength * 1000).round()] = Color.from(
+      red: r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      green: g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      blue: b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      alpha: 1.0,
     );
   }
 
-  return MaterialColor(color.value, swatch);
+  return MaterialColor(color.toARGB32(), swatch);
 }
